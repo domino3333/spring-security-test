c4840d38c3fe8649e691f9822b72a22d37dcb4d3
domino3333 <busster@naver.com>
2026-02-19 12:01:47 +0900

    config에 인가정책 추가 / 경로별로 권한 설정

--- src/main/java/com/zeus/config/SecurityConfig.java
+++ src/main/java/com/zeus/config/SecurityConfig.java
@@ -17,14 +17,25 @@ public class SecurityConfig {
 	
 	// 인증과 인가를 필터링할거임
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		log.info("----------------security config"+http);
+	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+		log.info("----------------security config"+httpSecurity);
 		
 		//1. csrf 토큰 비활성화
-		http.csrf(csrf->csrf.disable());
-		//2. 기본 로그인 폼은 스프링시큐리티에서 제공하는 것을 쓰겠다
-		http.formLogin(Customizer.withDefaults());
+		httpSecurity.csrf(csrf->csrf.disable());
+
 		
-		return null;
+		//2. 인가 정책
+		httpSecurity.authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/board/list").permitAll()          // 게시판 목록: 누구나
+	            .requestMatchers("/board/register").hasRole("MEMBER") // 게시판 등록: 회원만
+	            .requestMatchers("/notice/list").permitAll()         // 공지사항 목록: 누구나
+	            .requestMatchers("/notice/register").hasRole("ADMIN") // 공지사항 등록: 관리자만
+	            .anyRequest().authenticated()                         // 그 외 모든 요청은 인증 필요
+	        );
+		
+		//3. 기본 로그인 폼은 스프링시큐리티에서 제공하는 것을 쓰겠다
+		httpSecurity.formLogin(Customizer.withDefaults());
+		
+		return httpSecurity.build();
 	}
 }
